a1313ef83561ca97df4100556f7bf768f1d7531e
Pâmela Inácio Almeida <36995721+pamelaialmeida@users.noreply.github.com>
2024-11-28 14:45:16 -0800

    Create setup-jenkins.sh

---
+++ setup-jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo dnf -y install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run -u root -p 80:8080 --name jenkins-server -d -v /var/run/docker.sock:/var/run/docker.sock -v /home/ec2-user:/var/jenkins jenkins/jenkins:latest
+sudo docker exec jenkins-server apt update
+sudo docker exec jenkins-server apt install -y curl
+sudo docker exec jenkins-server curl -fsSL https://get.docker.com -o get-docker.sh
+sudo docker exec jenkins-server sh get-docker.sh
+sudo docker exec jenkins-server chmod 666 /var/run/docker.sock
